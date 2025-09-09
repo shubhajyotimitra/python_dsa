e2be064bb7618f6862e91c5ad02a16494877064f
shubhajyotimitra <subhamm482@gmail.com>
2025-09-09 20:19:15 +0530

    match case statement

---
+++ day14.py
@@ -0,0 +1,23 @@
+x = 4
+
+match x:
+    case 0:
+        print("x is zero")
+    case 4 if x % 2 == 0:
+        print("x % 2 == 0 and case is 4")
+    case _ if x <10:
+        print("x is less than 10")
+
+
+
+x = int(input("enter the value of x:"))
+
+match x:
+    case 0:
+        print("x is zero")
+    case 4 if x % 2 == 0:
+        print("the value is divisible by 2")
+    case _:
+        print("x is less than 10")
+
+
