acb851187f4f657615bd3247c21a5a8b180ec311
shubhajyotimitra <subhamm482@gmail.com>
2025-09-10 19:46:32 +0530

    for loops

---
+++ day15.py
@@ -0,0 +1,26 @@
+# for loops
+name = "shubhajyoti"
+for i in name :
+    print(i)
+    if i=='i':
+     print("he is doing python programming")         
+
+
+colors = ["red","green","blue","yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+#for one range:
+for k in range(5):
+    print(k+1)
+
+#for two range:
+for k in range(1,10):
+    print(k+1)
+
+#for three range:
+for k in range(1,20,2):
+    print(k+1)       
+        
