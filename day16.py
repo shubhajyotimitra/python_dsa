2e77f4229327e79463f5fa6e2080fdaf7c63c0a4
shubhajyotimitra <subhamm482@gmail.com>
2025-09-11 01:27:01 +0530

    while loop

---
+++ day16.py
@@ -0,0 +1,35 @@
+#while loop
+i = 0 
+while (i<3):
+    print(i)
+    i = i+1
+print("done with the loop")
+
+
+
+i= int(input("enter a number:"))
+while (i<=10):
+    i= int(input("enter a number:"))
+    print(i)
+print("done with the loop")
+
+
+#decrementing while loop
+
+count = 5
+while (count>0):
+    print(count)
+    count = count-1
+else:
+    print("i am inside data")
+
+
+
+
+      
+
+
+
+
+
+
