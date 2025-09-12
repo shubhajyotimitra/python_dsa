b3ba9b37ae2304b103bd43cc8b75626eaca9d753
shubhajyotimitra <subhamm482@gmail.com>
2025-09-12 19:37:09 +0530

    functions in python

--- day18.py
+++ day18.py
@@ -1,4 +1,9 @@
 #functon in python
+#functions are of two types:
+#user defines functions and built in functions 
+
+
+
 def calculateGmean(a,b):
     mean = (a*b)/(a+b)
     print(mean)
@@ -25,3 +30,39 @@ else:
 # gmean2 = (c*d)/(c+d)
 # print(gmean2)
 calculateGmean(c,d)
+
+
+#for if condition we can also make functions though
+
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("first number is greater")
+    else:
+        print("second number is greater is equal")  
+
+def isLesser(a,b):
+    if(a<b):
+        print("first number is lesser")
+    else:
+        print("second number is lesser")    
+                  
+
+a=9
+b=8
+isGreater(a,b)
+
+isLesser(a,b)
+
+calculateGmean(a,b)
+
+c=8
+d=9
+isGreater(c,d)
+
+isLesser(c,d)
+
+calculateGmean(c,d)
+
