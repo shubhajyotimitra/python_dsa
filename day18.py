357f56bf0b0b44fa4cd0a37f8b7ddb78e0d60b20
shubhajyotimitra <subhamm482@gmail.com>
2025-09-12 19:11:38 +0530

    functions is python

---
+++ day18.py
@@ -0,0 +1,27 @@
+#functon in python
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+
+a=9
+b=8
+if (a>b):
+    print("first number is greater")
+else:
+    print("second number is greater or equal")    
+calculateGmean(a,b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+
+c = 8 
+d = 74
+if (c>d):
+    print("first number is greater")
+else:
+    print("second number is greater or equal")    
+
+
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+calculateGmean(c,d)
