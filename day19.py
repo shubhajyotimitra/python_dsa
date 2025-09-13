d753ff68d24f783f5b236f35d7856d9821051564
shubhajyotimitra <subhamm482@gmail.com>
2025-09-13 11:32:40 +0530

    functions argument in python

--- day19.py
+++ day19.py
@@ -1,26 +1,75 @@
-#fuctions arguments in python
+# #fuctions arguments in python
 
-def average(a,b):
-    print("the average is ", (a+b)/2)
+# def average(a,b):
+#     print("the average is ", (a+b)/2)
 
-average(4,6) 
-
-
-#there are four types of argument that we can provide in a function:
-# default arguments, keyword arguments,variable length argument,required arguments
-
-#default argument
-def average(a=4,b=6):
-    print("the average is ", (a+b)/2)
-average()
+# average(4,6) 
 
 
-def name(fname, mname = "john", lname= "whatson"):
-    print("hello," , fname,mname,lname)
+# #there are four types of argument that we can provide in a function:
+# # default arguments, keyword arguments,variable length argument,required arguments
 
-name("amy")   
+# #default argument
+# def average(a=4,b=6):
+#     print("the average is ", (a+b)/2)
+# average()
 
-#keyword argument
-def average(a=9 ,b=1):
-    print("the average is ", (a+b)/2)
-average(b =9, a=21)    
+
+# def name(fname, mname = "john", lname= "whatson"):
+#     print("hello," , fname,mname,lname)
+
+# name("amy")   
+
+# #keyword argument
+# def average(a=9 ,b=1):
+#     print("the average is ", (a+b)/2)
+# average(b =9, a=21)    
+
+
+# #required argument
+# def average(a ,b=1):
+#     print("the average is ", (a+b)/2)
+# average(b =9, a=21) 
+
+
+# def average(a ,b,c=1):
+#     print("the average is ", (a+b)/2)
+# average(b =9, a=20)
+
+
+# #variable length argument
+def average(*numbers):
+    print(type(numbers))
+    sum=0
+
+    for i in numbers:
+        sum= sum+i
+    print("average is: ",sum / len(numbers))
+average(5,6,1)    
+
+
+
+def name(**name):
+    
+    print(type(name))
+    print("hello", name["fname"],name["mname"], name["lname"])
+
+name(mname= "buchaman", lname = "barnes",fname="james")
+
+
+
+#return statement
+def average(*numbers):
+    # print(type(numbers))
+    sum=0
+
+    for i in numbers:
+        sum= sum+i
+    # print("average is: ",sum / len(numbers))
+    return sum / len(numbers)
+c= average(5,6,7,1)
+print(c)
+
+
+
+
