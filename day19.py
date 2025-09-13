fcc8563fd55e593b0d52d1462b1a7480640f8c2f
shubhajyotimitra <subhamm482@gmail.com>
2025-09-13 15:42:36 +0530

    introduction to list

--- day19.py
+++ day19.py
@@ -1,43 +1,43 @@
-# #fuctions arguments in python
+#fuctions arguments in python
 
-# def average(a,b):
-#     print("the average is ", (a+b)/2)
+def average(a,b):
+    print("the average is ", (a+b)/2)
 
-# average(4,6) 
-
-
-# #there are four types of argument that we can provide in a function:
-# # default arguments, keyword arguments,variable length argument,required arguments
-
-# #default argument
-# def average(a=4,b=6):
-#     print("the average is ", (a+b)/2)
-# average()
+average(4,6) 
 
 
-# def name(fname, mname = "john", lname= "whatson"):
-#     print("hello," , fname,mname,lname)
+#there are four types of argument that we can provide in a function:
+# default arguments, keyword arguments,variable length argument,required arguments
 
-# name("amy")   
-
-# #keyword argument
-# def average(a=9 ,b=1):
-#     print("the average is ", (a+b)/2)
-# average(b =9, a=21)    
+#default argument
+def average(a=4,b=6):
+    print("the average is ", (a+b)/2)
+average()
 
 
-# #required argument
-# def average(a ,b=1):
-#     print("the average is ", (a+b)/2)
-# average(b =9, a=21) 
+def name(fname, mname = "john", lname= "whatson"):
+    print("hello," , fname,mname,lname)
+
+name("amy")   
+
+#keyword argument
+def average(a=9 ,b=1):
+    print("the average is ", (a+b)/2)
+average(b =9, a=21)    
 
 
-# def average(a ,b,c=1):
-#     print("the average is ", (a+b)/2)
-# average(b =9, a=20)
+#required argument
+def average(a ,b=1):
+    print("the average is ", (a+b)/2)
+average(b =9, a=21) 
 
 
-# #variable length argument
+def average(a ,b,c=1):
+    print("the average is ", (a+b)/2)
+average(b =9, a=20)
+
+
+#variable length argument
 def average(*numbers):
     print(type(numbers))
     sum=0
---
+++ day20.py
@@ -0,0 +1,65 @@
+# list
+# list are ordered collection of data types
+# list can be changed but tuples cant be changed
+# in list different data types can be stored
+
+
+l = [3,5,6]
+print(l)
+print(type(l))
+
+
+marks =[30,35,37,"shubhajyoti"]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+
+print(marks[-3])#negative index
+print(marks[len(marks)-3])#positive index
+print(marks[5-3])#positive index
+print(marks[2])#positive index
+
+if 7 in marks:
+    print("yes")
+else:print ("no")
+if "ha" in "shubhajyoti":
+    print("yes")
+else:
+    print("no")
+
+
+print(marks) 
+print(marks[1:-1])
+print(marks[1:3:2])   
+
+
+#question:1
+name=["c","r","a","z","y","g","u","y"]
+print(name)
+print(type(name))
+if "g" in name and "u"in name :
+    print("yes")
+else:
+    print("no")
+
+#question:2
+name =["crazyguy"]
+print(name)
+if "gu" in "crazyguy":
+    print("gu is present in crazyguy")
+else:
+    print("gu is not present") #same thing apply ffor string as well
+
+
+
+
+
+
+
+
+
+
+
+
