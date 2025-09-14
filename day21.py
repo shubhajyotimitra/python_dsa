8f36b53a84a4d91dd76edf57ba0a68f689accb36
shubhajyotimitra <subhamm482@gmail.com>
2025-09-14 10:00:22 +0530

    lists method

---
+++ day21.py
@@ -0,0 +1,29 @@
+#list method
+
+l =[11,45,1,2,4,6,1]
+print(l)
+# l.append(7)
+
+# l.sort(reverse=True)
+
+# l.reverse()
+
+# print(l.index(1))
+
+# print(l.count(1))
+
+# m=l
+# m[0]=0
+
+# m=l.copy()
+# m[0]=0
+# print(l)
+
+# l.insert(1,199)
+# print(l)
+
+m=[900,100,1100]
+# l.extend(m)
+k=l+m
+print(k)
+print(l)
