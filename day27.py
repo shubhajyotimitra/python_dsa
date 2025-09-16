c427bf5709832c3c4c3a8cf91472b6dd7c340517
shubhajyotimitra <subhamm482@gmail.com>
2025-09-16 19:55:13 +0530

    Docstring in python

---
+++ day27.py
@@ -0,0 +1,14 @@
+#doc strings
+def square(n):
+    ''' Takes in a number n , returns the square of n ''' #it is a doc string
+#doc string are placed in right below the function name or right above the function body
+    
+    print(n**2)
+square(5) 
+print(square.__doc__)  
+
+
+
+#PEP 8 - python enhancement proposal
+#it is a set of guidelines for writing code in python
+#its main aim is to improve the consistency,maintainable,readability, of code
