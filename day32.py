50f1c608cf13c7571a056d3502ff44bc1d6349a7
shubhajyotimitra <subhamm482@gmail.com>
2025-09-18 09:17:32 +0530

    dictionaries methods in python

---
+++ day32.py
@@ -0,0 +1,27 @@
+#Dictionaries methods
+
+ep1={122:45,123:89,567:69,670:69}
+ep2={222:67,566:90}
+ep1.update(ep2)
+print(ep1)
+
+#in python sets are unordered collection but dictionaries are ordered collection
+
+ep3={122:45,123:89,567:69,670:69}
+ep4={222:67,566:90}
+ep3.clear()
+print(ep3)
+empt = {}
+print(empt)
+
+
+ep1={122:45,123:89,567:69,670:69}
+ep2={222:67,566:90}
+ep1.pop(122)
+ep1.popitem()
+print(ep1)
+
+# del ep1[567]. :it is used to delete a particular key value pair
+
+
+
