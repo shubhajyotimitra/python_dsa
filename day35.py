1187de56a415134cc56ba5d01c1f8c826717bed9
shubhajyotimitra <subhamm482@gmail.com>
2025-09-20 18:31:09 +0530

    finally keyword in python

--- day35.py
+++ day35.py
@@ -10,3 +10,21 @@ except:
 finally:
     print("I am always executed")
 
+    
+
+def func1():
+    try:
+        l = [1,5,6,7]
+        i = int(input("Enter the index:"))
+        print(l[i])
+    except:
+        print("Some error occured")
+        return 0 
+    finally:
+        print("I am always executed")
+
+x= func1()
+print(x)
+
+#finally keyword is always executed whether exception is there or not.
+#it is generally used to close the file or release the resource.
