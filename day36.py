9ebdde536ea8ebcf3e54c4e3ef5208219928bf95
shubhajyotimitra <subhamm482@gmail.com>
2025-09-21 19:13:05 +0530

    raising custom errors in python

---
+++ day36.py
@@ -0,0 +1,8 @@
+#coutom errors in python
+a = int(input("enter any value between 5 and 9 :"))
+
+if (a<5 or a>9):
+    raise ValueError ("Value should be between 5 and 9")
+
+#in python we can raise coutom errors bu using the raise keyword.
+
