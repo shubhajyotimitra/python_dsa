5da52c2ddc81227870296bf3ae2194298a1a69f8
shubhajyotimitra <subhamm482@gmail.com>
2025-09-24 19:19:45 +0530

    Enumerate functions in python

--- day40.py
+++ day40.py
@@ -7,3 +7,22 @@ for mark in marks:
     if(index==3):
         print("osm shubh")
     index +=1
+    
+    
+    
+    
+marks = [12,36,32,98,12,45,1,4]
+
+for index, mark in enumerate(marks):
+    print(mark)
+    if(index==3):
+        print("osm shubh!!")
+
+    
+fruits = ['apple','banana','mango','grapes']
+for index, fruits in enumerate(fruits):
+    print(index, fruits)
+    if(index==1):
+        print("i love banana")
+
+
