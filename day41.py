ff9bb088c4b9a1c1aa974cf34dbd892c75629c67
shubhajyotimitra <subhamm482@gmail.com>
2025-09-25 16:05:42 +0530

    virtual environment in python

---
+++ day41.py
@@ -0,0 +1,7 @@
+#Virtual Environment
+#A virtual environment is a tool used to isolate specific python environment on a single machine
+#it also allowing you to work on multiple projects with different dependencies and packages without conflicts.
+
+#in this we got to know how to install virtual environment and inside the virtual environment i can install anything according to my choise.
+
+#once the virtual environment is activated, any packages that you install using pip will be installed in the virtual environment
