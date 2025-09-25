a71019ae1de9f4b034d03c585c51931a2afaaa91
shubhajyotimitra <subhamm482@gmail.com>
2025-09-25 16:52:23 +0530

    virtual environment in python

--- day41.py
+++ day41.py
@@ -1,7 +1,23 @@
-#Virtual Environment
+#Virtual Environment:
 #A virtual environment is a tool used to isolate specific python environment on a single machine
 #it also allowing you to work on multiple projects with different dependencies and packages without conflicts.
 
-#in this we got to know how to install virtual environment and inside the virtual environment i can install anything according to my choise.
 
+#in this we got to know how to install virtual environment and inside the virtual environment i can install anything according to my choise.
 #once the virtual environment is activated, any packages that you install using pip will be installed in the virtual environment
+#how to activate virtual environment: source myenv/bin/activate
+
+#suppose if i have to give someone my project including all of my packages used in it then ,
+#The "requirement.txt"file is used.
+#in this , while we are in my own virtual environment by this command "pip freeze" we can see how many files we have installed in it 
+#also by this command "pip freeze > requirements.txt"
+#after that if someone runs this command "pip install -r requirements.txt"
+
+import pandas as pd
+print(pd.__version__)
+#by this we got to know which version is install in the python global environment 
+
+#also if i have to check which version is install in virtual environment 
+#what we have to do is : firstly we have to activate our virtual environment 
+#after that we have to write:
+#                            python,import pandas as pd,pd.__version__
