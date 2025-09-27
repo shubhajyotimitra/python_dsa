16f54fe7066ab9d5543a895c862bb95c3f948bc2
shubhajyotimitra <subhamm482@gmail.com>
2025-09-27 11:10:23 +0530

    how to import works in python

--- day42.py
+++ day42.py
@@ -11,5 +11,19 @@ from math import *
 result = sqrt(9)
 print(result)
 print(pi)
+#the above snippet is generally not reccomanded as it can lead to confusion and make it harder to understand where specific functions and variable are coming from.
 
 
+import math as m
+result = m.sqrt(9)* m.pi
+print(result)
+#the above as keyword is can be useful when you want to use a shorter or more descriptive name for a module
+
+#dir function
+import math
+print(dir(math))
+#in this we got to know the names of all the functions and variables defined in a module.
+
+import math
+print(math.nan,type(math.nan))
+#by this we can also find the functions type.
