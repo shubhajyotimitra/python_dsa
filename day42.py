3bace30297b97cc975c28cae04f0d4f1af6aa167
shubhajyotimitra <subhamm482@gmail.com>
2025-09-27 10:57:31 +0530

    how to import work in python

---
+++ day42.py
@@ -0,0 +1,15 @@
+#how to imort work in python
+import math
+result = math.sqrt(9)
+print(result)
+
+from math import sqrt,pi
+result = sqrt(80) * pi
+print(result)
+
+from math import *
+result = sqrt(9)
+print(result)
+print(pi)
+
+
