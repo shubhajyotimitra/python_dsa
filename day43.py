be7b04d9b8668000e2d8f190bf14c795d261000b
shubhajyotimitra <subhamm482@gmail.com>
2025-09-28 11:30:05 +0530

    if__name__==__file_name_ in python

--- day43.py
+++ day43.py
@@ -6,3 +6,6 @@ shubh.welcome()
 #as i have already made a file name "shubh.py" and i have made another file in an order to import the things i have written in the "shubh file"
 #but here is the problem when i write "shubh.welcome" then in output wil print two times 
 #also if i remove "shubh.welcome()" then it will also continue and print the output only one time
+
+#it is mainly used to control the execution of code depending on whether the file is being run directly or imported as a module into another script.
+#it also helps you separate executable code from importable code.
