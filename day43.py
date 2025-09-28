69035972ac3918a81c9d82d7e4be10e0b8c01fd9
shubhajyotimitra <subhamm482@gmail.com>
2025-09-28 11:25:12 +0530

    if__name__==__file_name_ in python

---
+++ day43.py
@@ -0,0 +1,8 @@
+#if_name_=="_main_" in python
+
+import shubh
+shubh.welcome()
+
+#as i have already made a file name "shubh.py" and i have made another file in an order to import the things i have written in the "shubh file"
+#but here is the problem when i write "shubh.welcome" then in output wil print two times 
+#also if i remove "shubh.welcome()" then it will also continue and print the output only one time
--- shubh.py
+++ shubh.py
@@ -1,3 +1,8 @@
+# def welcome():
+#     print("heyy you are welcome my friend")
+# shubh="A good boy"
+
 def welcome():
-    print("heyy you are welcome my friend")
-shubh="A good boy"
+    print("hey you are welcome from shubh")
+    if __name__== "__day43__":
+     welcome()
