058650988902b9ad6b15360756bb7cded7d487b3
shubhajyotimitra <subhamm482@gmail.com>
2025-10-03 21:47:38 +0530

    KBC question

--- day25.py
+++ day25.py
@@ -1,3 +1,5 @@
+# KBC question solve:
+
 #create a program capable of displaying question to the user like KBC.
 #use list data type to store the questions and their correct answers.
 #Display the final amount the perso is taking home after playing the game.
