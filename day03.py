eb26e1607ba0a9536f1e190ba00bc089add54c8c
shubhajyotimitra <subhamm482@gmail.com>
2025-10-09 20:18:19 +0530

    escape sequence

--- day03.py
+++ day03.py
@@ -2,4 +2,4 @@ print("hello world")
 #escape sequence
 print("heyy i am a \"good boy\" \nand this viewer is also a good boy")
 print ("Hey",6,7,sep="~",end="009\n")
-print("shubhajyoti mitra")
+print("Shubhajyoti mitra")
