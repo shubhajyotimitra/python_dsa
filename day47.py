c4b7627c2d5b701c0747511e39fbd315b18469ec
shubhajyotimitra <subhamm482@gmail.com>
2025-10-10 21:42:14 +0530

    file IO in python

--- day47.py
+++ day47.py
@@ -1,14 +1,20 @@
 #file IO in python:
 
 #READING A FILE 
-# f= open('myfile.txt','r')
-# print(f)
-# text = f.read()
-# print(text)
-# f.close()
+f= open('myfile.txt','r')
+print(f)
+text = f.read()
+print(text)
+f.close()
 
 #WRITING A FILE
+f=open('myfile2.txt', 'w')
+f.write('hello, world!')
+f.close()
+
+#if we open the file in the form of append then the number of times we run program , the text file will add on.
 f=open('myfile2.txt', 'a')
 f.write('hello, world!')
 f.close()
-#if we open the file in the form of append then the number of times we run program , the text file will add on.
+with open('myfile2.text', 'a') as f:
+    f.write("heyy i am inside with")
