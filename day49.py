67212546d8706857f7be51a770b27e738df694d5
shubhajyotimitra <subhamm482@gmail.com>
2025-10-13 22:25:27 +0530

    seek(), tell() and other functions

--- day49.py
+++ day49.py
@@ -1,16 +1,16 @@
 #seek(), tell() and other functions
-# with open('file.txt' , 'r') as f:
-#     print(type(f))
-#     #Move to the 10th byte in the file
-#     f.seek(10)
+with open('file.txt' , 'r') as f:
+    print(type(f))
+    #Move to the 10th byte in the file
+    f.seek(10)
 
-#     #read the next 5 bytes
-#     print(f.tell())
-#     #the tell() functions returns the current position within the files in bytes. 
-#     #This can be useful for keeping track of your location within the file or for seeking to a specific position relative to the current position .
+    #read the next 5 bytes
+    print(f.tell())
+    #the tell() functions returns the current position within the files in bytes. 
+    #This can be useful for keeping track of your location within the file or for seeking to a specific position relative to the current position .
 
-#     data = f.read(5)
-#     print(data)
+    data = f.read(5)
+    print(data)
 
 with open('sample.txt' , 'w') as f:
     f.write('Hello World ')
