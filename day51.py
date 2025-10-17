6c229a2e051e7c6413cc8ebdd71e141c1647c008
shubhajyotimitra <subhamm482@gmail.com>
2025-10-17 23:51:50 +0530

    map, filter and reduce in python

--- day51.py
+++ day51.py
@@ -1,13 +1,16 @@
 #Map, filter and reduce in python:
-# def cube(x):
-#     return x**3
-# print(cube(3))
-# l = [1,2,3,4,5]
-# newl = []
-# for item in l:
-#     newl.append(cube(item))
-# print(newl)
+#simplest way to understand map function is to see it as a for loop that applies a function to each item in an iterable (like a list) and returns a new iterable (like a list) with the results.
+#by using defined function:
+def cube(x):
+    return x**3
+print(cube(3))
+l = [1,2,3,4,5]
+newl = []
+for item in l:
+    newl.append(cube(item))
+print(newl)
 
+#by using map function and defined function:
 def cube(x):
     return x**3
 print(cube(3))
