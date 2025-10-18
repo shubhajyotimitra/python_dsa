ef4901c8235f884d45c44bb302fbac08c775a4ee
shubhajyotimitra <subhamm482@gmail.com>
2025-10-18 23:14:40 +0530

    map, filter and reduce in python

--- day51.py
+++ day51.py
@@ -29,6 +29,9 @@ print(newnewl)
 l = [1,2,3,4,5,6,7,8,9]
 newl = list(map(lambda x: x**3,l))
 print(newl)
+numbers = [1, 2, 3, 4, 5, 6]
+even = list(filter(lambda x: x % 2 == 0, numbers))
+print(even)
 
 #reduce function:
 #by using lambda function 
