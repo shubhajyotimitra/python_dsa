f770dd45ff986223bc2f0146047e3319a1a01a73
shubhajyotimitra <subhamm482@gmail.com>
2025-10-19 20:54:35 +0530

    is vs = in python

--- day52.py
+++ day52.py
@@ -8,3 +8,9 @@ a = (1,2)
 b = (1,2)
 print(a is b)
 print(a == b)
+
+a = None
+b = None 
+print(a is b)# exact location of object in memory
+print(a is None)# exact location of object in memory
+print(a == b)# value comparison
