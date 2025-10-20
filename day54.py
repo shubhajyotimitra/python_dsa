9fad959a29846dd1708f4cb19147a52faf8e5711
shubhajyotimitra <subhamm482@gmail.com>
2025-10-20 20:22:08 +0530

    Introduction to OOPs in python

---
+++ day54.py
@@ -0,0 +1,24 @@
+#Introduction to OOPs in python :
+def hello():
+    print("Hello")
+hello ()
+
+sales1= 6000
+profit1 = 2000
+ad1 = 1000
+#shubh.sales
+
+sales2 = 6000
+profit2 = 2000
+ad2= 1000
+#tom.sales
+
+sales3 = 6000
+profit3 = 2000
+ad3 = 1000
+#shubham.sales
+
+#RailwayForm ---> class [blueprint]
+#shubh --> shubh's railway form --> object [entitity]
+#tom --> tom's railway form --> object [entitity]
+#shubham --> shubham's railway form --> object [entitity]
