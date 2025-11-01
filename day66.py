a759cad33d2cc494acb727607ea27782cf4fafce
shubhajyotimitra <subhamm482@gmail.com>
2025-11-01 20:13:55 +0530

    Exercise question

---
+++ day66.py
@@ -0,0 +1,8 @@
+# Exercise question :
+# 1- Write a program to clear the clutter inside a folder on your computer.
+# 2 You should use os module to rename all the png images from 1.png all the way till n.png where n is the number of png files in that folder. Do the same for other file formats. For example:
+# 3 sfdsf .png --> 1.png
+# 4 vfsf.png   --> 2.png
+# 5 this.png   --> 3.png
+# 6 design.png --> 4.png
+# 7 name.png   --> 5.png
