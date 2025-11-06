ee1df4fc5bd191fc8ac69cac006e6516952e3529
shubhajyotimitra <subhamm482@gmail.com>
2025-11-06 19:08:15 +0530

    Clear clutter problem solving day-66

---
+++ day73.py
@@ -0,0 +1,12 @@
+# Clear clutter problem solving day-66
+
+
+import os 
+
+files = os.listdir("cluttered")
+i = 1
+for file in files:
+    if file.endswith(".png"):
+     print(file)
+os.rename(f"cluttered/{file}" , f"cluttered/{i}.png")
+i = i+1
