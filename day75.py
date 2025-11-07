7cba22e091c4943cbc9d04733d6691be70488c48
shubhajyotimitra <subhamm482@gmail.com>
2025-11-07 19:12:44 +0530

    Operator Overloading in python

---
+++ day75.py
@@ -0,0 +1,4 @@
+# Operator Overloading in python :
+# Operator overloading is a feature in python that allows deveelopers to redefine the behaviour of mathematical and comparison operators for custom data types.
+# this means that you can use the standard mathematical operators (+,-,*,/,etc)
+# comparisonn operators (<,>,==,etc.) in your own classes, just as you would for built-in data types like int,float, and str.
