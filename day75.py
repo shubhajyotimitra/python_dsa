5fc62b327c6e8c680482527b4e712f382caee248
shubhajyotimitra <subhamm482@gmail.com>
2025-11-08 20:26:03 +0530

    Operator Overloading in python

--- day75.py
+++ day75.py
@@ -10,6 +10,28 @@ class Vector :
         self.k = k
     def __str__(self):
         return f"{self.i}i + {self.j}k + {self.k}k"
-v = Vector(3,5,6)
-print(v)          
-    
+    def __add__(self,x):
+        return f"{self.i +x.i}i + {self.j+x.j}k + {self.k+x.k}k"
+v1= Vector(3,5,6)
+print(v1)          
+v2= Vector(3,5,6)
+print(v2)     
+print(v1 +v2) 
+print(type(v1+v2))
+# for the above code the type of the output is str 
+# in an order to get the output as the real vector we have to do this like below :
+class Vector :
+    def __init__(self,i,j,k):
+        self.i = i
+        self.j = j
+        self.k = k
+    def __str__(self):
+        return f"{self.i}i + {self.j}k + {self.k}k"
+    def __add__(self,x):
+        return Vector(self.i + x.i, self.j + x.j , self.k + x.k)
+v1= Vector(3,5,6)
+print(v1)          
+v2= Vector(3,5,6)
+print(v2)     
+print(v1 +v2) 
+print(type(v1+v2))
