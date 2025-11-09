d65df21af54a245af88a0ccb7469f0806f16af6f
shubhajyotimitra <subhamm482@gmail.com>
2025-11-09 19:33:33 +0530

    Single inheritance

---
+++ day76.py
@@ -0,0 +1,22 @@
+# Single inheritance :
+# Single inheritance is a type of inheritance in python Single Inheritance means a child class (derived class) inherits properties and behaviors (attributes and methods) from only one parent class (base class).
+# This allows code reusability and the extension of existing functionality.
+
+class Animal :
+    def __init__(self,name,species):
+        self.name = name 
+        self.species = species
+
+    def make_sound(self):
+        print("Sound made by the animal")    
+class Dog (Animal):
+    def __init__(self, name, breed):
+         Animal.__init__(self,name,species="Dog")
+         self.breed = breed
+    def make_sound(self):
+        print("Bark!")    
+
+d = Dog("Dog","Doggerman")
+d.make_sound()
+a = Animal("Dog","Dog")
+a.make_sound()             
