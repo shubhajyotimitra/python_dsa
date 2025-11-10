7cdb9d4c7d30ad87cacb8281ea0675f0aec409f2
shubhajyotimitra <subhamm482@gmail.com>
2025-11-10 19:45:06 +0530

    Multiple inheritance

---
+++ day77.py
@@ -0,0 +1,12 @@
+# Multiple inheritance:
+# Multiple Inheritance means a child class inherits from more than one parent class.
+# That is, a derived class can access attributes and methods from multiple base classes.
+# Syntax:
+# class Parent1:
+#     pass
+
+# class Parent2:
+#     pass
+
+# class Child(Parent1, Parent2):
+#     pass
