59b05477085d5cac4f509716d2a63c1e5e20a6d4
shubhajyotimitra <subhamm482@gmail.com>
2025-11-12 23:33:07 +0530

    Multilevel Inheritance

---
+++ day78.py
@@ -0,0 +1,7 @@
+# Multilevel Inheritance :
+# Multilevel Inheritance means when a class is derived from another derived class — i.e., inheritance happens in multiple levels.
+# So,
+# Class A → Base class
+# Class B → Derived from A
+# Class C → Derived from B
+# This forms a chain of inheritance.
