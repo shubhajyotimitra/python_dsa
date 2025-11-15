a5fa6fa11b4397e8cc8eba56838014fc28437269
shubhajyotimitra <subhamm482@gmail.com>
2025-11-15 20:14:15 +0530

    Hybrid and hierarchical Inheritance in python

--- day79.py
+++ day79.py
@@ -3,6 +3,18 @@
 # Hybrid inheritance is a type of inheritance in Python where more than one type of inheritance (such as single, multiple, multilevel, or hierarchical) is combined together in the same program.
 # It forms a complex relationship between classes by mixing different inheritance patterns.
 
+class BaseClass:
+    pass
+
+class Derived1(BaseClass):
+    pass
+
+class Derived2(BaseClass):
+    pass
+
+class Derived3(BaseClass):
+    pass
+
 class A:
     def show(self):
         print("I am from Class A")
@@ -26,3 +38,4 @@ obj.show()
 
 # Check MRO (Method Resolution Order)
 print(D.__mro__)
+
