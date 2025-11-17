eec73f2af1a9644e4776192b031ef06ad0e4fa14
shubhajyotimitra <subhamm482@gmail.com>
2025-11-17 23:51:27 +0530

    Merge the PDF files problem solving day-74

--- day80.py
+++ day80.py
@@ -1,13 +1,22 @@
 # Merge the PDF files problem solving day-74
 
-from PyPDF2 import PdfMerger
+from pypdf import PdfMerger
 import os
 
 merger = PdfMerger()
-files = [file for file in os.listdir() if file.endswith(".pdf")]
+files = sorted([f for f in os.listdir() if f.lower().endswith(".pdf")])
+
+# Optional: print files that will be merged
+print("Merging these files in order:")
+for f in files:
+    print(" -", f)
 
 for pdf in files:
-    merger.append(pdf)
+    with open(pdf, "rb") as fh:
+        merger.append(fh)
 
-merger.write("merged-pdf.pdf")
+with open("merged-pdf.pdf", "wb") as out:
+    merger.write(out)
+
 merger.close()
+print("Done: merged-pdf.pdf created")
