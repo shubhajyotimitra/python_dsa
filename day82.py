b5b20184de17aaaa2f003dd58f2a174bc7e7cf62
shubhajyotimitra <subhamm482@gmail.com>
2025-11-18 21:54:52 +0530

    Time module in python

--- day82.py
+++ day82.py
@@ -25,4 +25,10 @@ print(4)
 time.sleep(3)
 print("This is printed after 3 seconds")
 
+#time.strftime() function :
+import time 
 
+t = time.localtime()
+formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
+
+print(formatted_time)
