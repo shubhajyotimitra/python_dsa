6fbfc09f67addb40902a9890bcac4559a66885d8
shubhajyotimitra <subhamm482@gmail.com>
2025-11-18 19:46:00 +0530

    Exercise question

---
+++ day81.py
@@ -0,0 +1,9 @@
+# 1. Write a program to pronounce list of names using win32 API.
+# 2. If you are given a list l as follows:
+# 3. '''python
+# 4.  l = ["Rahul", "Nishant", "Harry"]
+# 5.  '''
+# 6. Your program should pronouce:
+# 7. '''
+# 8 Shoutout to Rahul
+# 9 Shoutout to Nishant
