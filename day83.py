5da5a1011d14d5fa823dc2f0b0efe148953cfbd4
shubhajyotimitra <subhamm482@gmail.com>
2025-11-21 20:05:49 +0530

    Creating command line utility in python

--- day83.py
+++ day83.py
@@ -1,26 +1,27 @@
 # Creating command line utility in python:
+
 # A command line utility is a program that is designed to be run from the command line interface (CLI) of an operating system. It allows users to perform specific tasks or operations by entering commands and arguments in a terminal or command prompt.
 # A command-line utility in Python is simply a Python script that you can run from the terminal using commands—just like git, pip, or ls.
 # You can build one easily using argparse, click, or typer.
 
-# import argparse
+import argparse
 
-# def main():
-#     parser = argparse.ArgumentParser(description="Simple greeting CLI tool")
+def main():
+    parser = argparse.ArgumentParser(description="Simple greeting CLI tool")
 
-#     parser.add_argument("name", help="Enter your name")
-#     parser.add_argument("-u", "--uppercase", action="store_true",
-#                         help="Show the name in uppercase")
+    parser.add_argument("name", help="Enter your name")
+    parser.add_argument("-u", "--uppercase", action="store_true",
+                        help="Show the name in uppercase")
 
-#     args = parser.parse_args()
+    args = parser.parse_args()
 
-#     if args.uppercase:
-#         print(f"HELLO {args.name.upper()}!")
-#     else:
-#         print(f"Hello {args.name}")
+    if args.uppercase:
+        print(f"HELLO {args.name.upper()}!")
+    else:
+        print(f"Hello {args.name}")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 # Run this command in terminal to download any image from the internet:
 # curl https://www.sexyloops.com/movies/background.jpg --output shubh.jpg
