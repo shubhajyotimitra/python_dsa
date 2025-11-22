6c88beed065250f168df030fdc979c6078c98776
shubhajyotimitra <subhamm482@gmail.com>
2025-11-22 20:36:51 +0530

    Walrus operator in python

---
+++ day84.py
@@ -0,0 +1,10 @@
+# Walrus operator:
+# The walrus operator (:=) allows you to assign values to variables as part of an expression.
+# This can make your code more concise and readable by reducing the need for separate assignment statements.    
+# Example without walrus operator:
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = []
+for n in numbers:
+    squared = n ** 2
+    squared_numbers.append(squared)
+print(squared_numbers)      
