b3dd9a20f11ffc7c41a6e8415a630119910df439
shubhajyotimitra <subhamm482@gmail.com>
2025-11-23 19:46:10 +0530

    Walrus operator in python

--- day84.py
+++ day84.py
@@ -1,16 +1,32 @@
 # Walrus operator:
 # The walrus operator (:=) allows you to assign values to variables as part of an expression.
 # This can make your code more concise and readable by reducing the need for separate assignment statements.    
+
+
+a = True
+print(a:=False)  # Assigns False to a and prints it
+
+# Example of walrus operator in a while loop:
+numbers = [1, 2, 3, 4, 5]
+while (n:=len(numbers)) > 0:
+    print(numbers.pop())
+
+
+
 # Example without walrus operator:
+
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = []
 for n in numbers:
     squared = n ** 2
     squared_numbers.append(squared)
-print(squared_numbers)      
+print(squared_numbers)  
+
 # Example with walrus operator:
+
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = []        
 for n in numbers:
     squared_numbers.append(squared := n ** 2)
 print(squared_numbers)
+
