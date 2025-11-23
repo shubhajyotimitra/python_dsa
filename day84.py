5237c80dbc695349ace4a2d858e04e697a141435
shubhajyotimitra <subhamm482@gmail.com>
2025-11-23 19:59:54 +0530

    Walrus operator in python

--- day84.py
+++ day84.py
@@ -11,22 +11,34 @@ numbers = [1, 2, 3, 4, 5]
 while (n:=len(numbers)) > 0:
     print(numbers.pop())
 
+# Example without walrus operator 
+# foods = list()
+# while True:
+#     food = input ("what food do you like?:")
+#     if food == "quit":
+#         break
+#     foods.append(food)    
+
+# Example with walrus operator:
+foods = list()
+while (food := input("what food do you like?:")) != "quit":
+    foods.append(food)
 
 
 # Example without walrus operator:
 
-numbers = [1, 2, 3, 4, 5]
-squared_numbers = []
-for n in numbers:
-    squared = n ** 2
-    squared_numbers.append(squared)
-print(squared_numbers)  
+# numbers = [1, 2, 3, 4, 5]
+# squared_numbers = []
+# for n in numbers:
+#     squared = n ** 2
+#     squared_numbers.append(squared)
+# print(squared_numbers)  
 
-# Example with walrus operator:
+# # Example with walrus operator:
 
-numbers = [1, 2, 3, 4, 5]
-squared_numbers = []        
-for n in numbers:
-    squared_numbers.append(squared := n ** 2)
-print(squared_numbers)
+# numbers = [1, 2, 3, 4, 5]
+# squared_numbers = []        
+# for n in numbers:
+#     squared_numbers.append(squared := n ** 2)
+# print(squared_numbers)
 
