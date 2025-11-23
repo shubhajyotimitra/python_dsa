0c57af9ce14110baa180238ac3e76a41e040a9fd
shubhajyotimitra <subhamm482@gmail.com>
2025-11-23 20:02:30 +0530

    Walrus operator in python

--- day84.py
+++ day84.py
@@ -2,7 +2,7 @@
 # The walrus operator (:=) allows you to assign values to variables as part of an expression.
 # This can make your code more concise and readable by reducing the need for separate assignment statements.    
 
-
+# Example :-
 a = True
 print(a:=False)  # Assigns False to a and prints it
 
@@ -12,12 +12,12 @@ while (n:=len(numbers)) > 0:
     print(numbers.pop())
 
 # Example without walrus operator 
-# foods = list()
-# while True:
-#     food = input ("what food do you like?:")
-#     if food == "quit":
-#         break
-#     foods.append(food)    
+foods = list()
+while True:
+    food = input ("what food do you like?:")
+    if food == "quit":
+        break
+    foods.append(food)    
 
 # Example with walrus operator:
 foods = list()
@@ -26,19 +26,17 @@ while (food := input("what food do you like?:")) != "quit":
 
 
 # Example without walrus operator:
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = []
+for n in numbers:
+    squared = n ** 2
+    squared_numbers.append(squared)
+print(squared_numbers)  
 
-# numbers = [1, 2, 3, 4, 5]
-# squared_numbers = []
-# for n in numbers:
-#     squared = n ** 2
-#     squared_numbers.append(squared)
-# print(squared_numbers)  
-
-# # Example with walrus operator:
-
-# numbers = [1, 2, 3, 4, 5]
-# squared_numbers = []        
-# for n in numbers:
-#     squared_numbers.append(squared := n ** 2)
-# print(squared_numbers)
+# Example with walrus operator:
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = []        
+for n in numbers:
+    squared_numbers.append(squared := n ** 2)
+print(squared_numbers)
 
